083f102d7afe2c24d8776c8a1f5ba9884b77eef8
Jawed Alam <zjaweds@outlook.com>
2020-09-08 21:52:33 +0530

    Executed successfully for different approaches to solve the same problem.

--- areaOfCircleUsingFunction.c
+++ areaOfCircleUsingFunction.c
@@ -24,6 +24,161 @@ int radiusValidation(float);
 //___________________________________________________________________________________________________________
 
 
+//----------------------------------------------------------------------------------
+/*
+Following is the section of this program, written to calculate specified no of
+Circles with specified radius.
+Prerequisites are no of circles and their corresponding radius
+||||
+vvvv*/
+
+/*
+int main(){
+    float radius;
+    int nOfCircles;
+
+    printf("Here you can calculate area of circle with any radius.\n");
+    printf("How many circles are there ?: ");
+    scanf("%d",&nOfCircles);
+    if(nOfCircles<0){
+        printf("\n!Enter valid no of circles!\n");
+        printf("How many circles are there ?: ");
+        scanf("%d",&nOfCircles);
+    }
+    for(int iteration=0; iteration<nOfCircles; iteration++){
+        printf("\nEnter the radius of the circle whose area is to be calculated: ");
+        scanf("%f", &radius);//Reading radius from standard input
+        //Directly printing the calculated area without storing result to any other variable
+        printf("\nArea of the %dth circle with specified radius is: %f",iteration + 1,calcArea(radius));
+    }
+    return 0;
+}
+*/
+
+/*
+^^^^^
+|||||   Preceding is the section of this program, written to calculate specified no of
+Circles with specified radius.
+Prerequisites are no of circles and their corresponding radius
+*/
+//----------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------
+/*
+Following is the section of this program, written to calculate unspecified no of
+Circles with specified radius.
+Prerequisites is the radius of circles.
+When no more circles are there then enter radius of the circle as 0 (Zero).
+||||
+vvvv*/
+
+
+/*
+
+This task can be completed using for loop also by putting radius != 0 in coditional -
+expression of for loop
+
+int main(){
+    float radius;
+    printf("Enter 0 when no circles are left\nEnter radius of the circle: ");
+    scanf("%f",&radius);
+    while(radius){
+        //Directly printing the calculated area without storing result to any other variable
+        printf("\nArea of the circle with specified radius is: %f",calcArea(radius));
+        printf("\nEnter 0 when no circles are left\nEnter the radius of the circle whose area is to be calculated: ");
+        scanf("%f", &radius);//Reading radius from standard input
+    }
+return 0;
+}
+*/
+
+/*
+^^^^^
+|||||   Preceding is the section of this program, written to calculate unspecified no of
+Circles with specified radius.
+Prerequisites is the radius of circles.
+When no more circles are there then enter radius of the circle as 0 (Zero).
+*/
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+/*
+Following is the section of this program, written to calculate unspecified no of
+Circles with specified radius. And radius and area are to be stored in arrays
+for 100 circles, we can change no of circles by changing the NOFCIRCLE value
+from 100 to intended value. 
+Prerequisites is the radius of circles.
+When no more circles are there then enter radius of the circle as 0 (Zero).
+||||
+vvvv*/
+
+/*
+int main(){
+    float radius[NOFCIRCLE],area[NOFCIRCLE];
+    int count = 0;
+    printf("Enter the radius of the circle: ");
+    scanf("%f",radius);
+    while(radius[count] && count < NOFCIRCLE){
+        area[count]= calcArea(radius[count]);
+        printf("\nArea of the circle with specified radius is: %f",area[count]);
+        printf("\nEnter 0 when no circles are left\nEnter the radius of the circle whose area is to be calculated: ");
+        scanf("%f", &radius[count+1]);//Reading radius from standard input
+        count++;
+    }
+    return 0;
+}
+*/
+
+/*
+^^^^^
+|||||   Following is the section of this program, written to calculate unspecified no of
+Circles with specified radius. And radius and area are to be stored in arrays
+for 100 circles, we can change no of circles by changing the NOFCIRCLE value
+from 100 to intended value. 
+Prerequisites is the radius of circles.
+When no more circles are there then enter radius of the circle as 0 (Zero).
+*/
+//----------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------
+/*
+Following is the section of this program, written to calculate unspecified no of
+Circles with specified radius. And radius and area are to be stored in arrays
+for 100 circles, we can change no of circles by changing the NOFCIRCLE value
+from 100 to intended value. 
+Prerequisites is the radius of circles.
+When no more circles are there then enter radius of the circle as 0 (Zero).
+||||
+vvvv*/
+
+/*
+int main(){
+    float radius[NOFCIRCLE],area[NOFCIRCLE];
+    int count = 0;
+    printf("Enter the radius of the circle: ");
+    scanf("%f",radius);
+    while(radius[count] && count < NOFCIRCLE){
+        area[count]= calcArea(radius[count]);
+        printf("\nArea of the circle with specified radius is: %f",area[count]);
+        printf("\nEnter 0 when no circles are left\nEnter the radius of the circle whose area is to be calculated: ");
+        scanf("%f", &radius[count+1]);//Reading radius from standard input
+        count++;
+    }
+    return 0;
+}
+*/
+
+/*
+^^^^^
+|||||   Following is the section of this program, written to calculate unspecified no of
+Circles with specified radius. And radius and area are to be stored in arrays
+for 100 circles, we can change no of circles by changing the NOFCIRCLE value
+from 100 to intended value. 
+Prerequisites is the radius of circles.
+When no more circles are there then enter radius of the circle as 0 (Zero).
+*/
+//----------------------------------------------------------------------------------
+
 struct Shape{
     float radius;
     float area;
